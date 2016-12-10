7620cd61b7f8afc49616203f858ae99be0800bd8
leeic007 <leecb007@outlook.com>
2016-12-10 10:06:43 +0800

    commit local project

---
+++ assets/js/search.js
@@ -0,0 +1,7 @@
+This the main code for the search, but I changed one line to fulfill a requirement I had. 
+.
+. Main Christian Fei search code
+.  
+searchResultTemplate: '<img src="{image}"></img><li><a href="{url}" title="{desc}">{title}</a></li>',  
+.  
+.  
